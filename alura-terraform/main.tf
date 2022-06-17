29b0f00af75dcb7f9719c59d522b774e5a602903
Felipe Alexandre <felipeas314@gmail.com>
2022-06-17 09:05:20 -0300

    feat: add instance ec2 and security group

--- alura-terraform/main.tf
+++ alura-terraform/main.tf
@@ -1,3 +1,32 @@
 provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+  count = 3
+  ami = "ami-coloquesuaami"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    Name = "dev${count.index}"
+  }
+  vpc_security_group_ids = ["sg-seusecuritygroup"]
+}
+
+resource "aws_security_group" "acesso-ssh" {
+  name = "acesso-ssh"
+  description = "acesso-ssh"
   
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["191.32.154.79/32"]
+  }
+  tags = {
+    Name = "ssh"
+  }
 }
